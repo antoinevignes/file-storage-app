9bdefd2b78033c1552251cbbe54cb5d8afc9454f
antoinevignes <168306302+antoinevignes@users.noreply.github.com>
2024-08-13 18:11:48 +0200

    Adding upload file dialog

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,7 +5,9 @@ export default defineSchema({
   files: defineTable({
     name: v.string(),
     orgId: v.string(),
+    fileId: v.id("_storage"),
   }).index("by_orgId", ["orgId"]),
+
   users: defineTable({
     tokenIdentifier: v.string(),
     orgIds: v.array(v.string()),
