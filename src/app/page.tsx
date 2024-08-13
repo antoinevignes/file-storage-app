e74974269d4a9c60e1606c9949b74d2e6265113b
antoinevignes <168306302+antoinevignes@users.noreply.github.com>
2024-08-13 19:32:27 +0200

    Added ability to delete files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 import UploadButton from "./upload-button";
+import { FileCard } from "./file-card";
 
 export default function Home() {
   const organization = useOrganization();
@@ -20,15 +21,16 @@ export default function Home() {
 
   return (
     <main className="container mx-auto pt-12">
-      <div className="flex justify-between items-center">
+      <div className="flex justify-between items-center mb-8">
         <h1 className="text-4xl font-bold">Your files</h1>
 
         <UploadButton />
       </div>
-
-      {files?.map((file) => {
-        return <div key={file._id}>{file.name}</div>;
-      })}
+      <div className="grid grid-cols-4 gap-4">
+        {files?.map((file) => {
+          return <FileCard key={file._id} file={file} />;
+        })}
+      </div>
     </main>
   );
 }
