1c3c0dcaceed232bac067cd2507714b87c92af40
antoinevignes <168306302+antoinevignes@users.noreply.github.com>
2024-08-15 17:04:06 +0200

    View toggle and table view

--- src/app/dashboard/_components/file-card.tsx
+++ src/app/dashboard/_components/file-card.tsx
@@ -5,159 +5,21 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Doc, Id } from "../../../../convex/_generated/dataModel";
+import { Doc } from "../../../../convex/_generated/dataModel";
 import { formatRelative } from "date-fns";
-import { fr } from "date-fns/locale";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {
-  DownloadIcon,
-  EllipsisVertical,
-  FileTextIcon,
-  GanttChartIcon,
-  ImageIcon,
-  StarHalf,
-  StarIcon,
-  TrashIcon,
-  UndoIcon,
-} from "lucide-react";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-} from "@/components/ui/alert-dialog";
-import { ReactNode, useState } from "react";
-import { useMutation, useQuery } from "convex/react";
+import { FileTextIcon, GanttChartIcon, ImageIcon } from "lucide-react";
+
+import { ReactNode } from "react";
+import { useQuery } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
-import { useToast } from "@/components/ui/use-toast";
 import Image from "next/image";
-import { Protect } from "@clerk/nextjs";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-
-function FileCardActions({
-  file,
-  isFavorited,
-}: {
-  file: Doc<"files"> & { url: string | null };
-  isFavorited: boolean;
-}) {
-  const deleteFile = useMutation(api.files.deleteFile);
-  const restoreFile = useMutation(api.files.restoreFile);
-  const toggleFavorite = useMutation(api.files.toggleFavorite);
-  const { toast } = useToast();
-
-  const [isConfirmOpen, setIsConfirmOpen] = useState(false);
-  return (
-    <>
-      <AlertDialog open={isConfirmOpen} onOpenChange={setIsConfirmOpen}>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action will mark the file for our deletion process. Files are
-              deleted periodically.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction
-              onClick={async () => {
-                await deleteFile({
-                  fileId: file._id,
-                });
-                toast({
-                  variant: "default",
-                  title: "File deleted successfully!",
-                });
-              }}
-            >
-              Continue
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <EllipsisVertical />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuItem
-            onClick={() => {
-              if (!file.url) return;
-              window.open(file.url, "_blank");
-            }}
-            className="flex gap-2 items-center cursor-pointer"
-          >
-            <DownloadIcon className="w-4 h4" /> Download
-          </DropdownMenuItem>
-
-          <DropdownMenuItem
-            onClick={() => {
-              toggleFavorite({
-                fileId: file._id,
-              });
-            }}
-            className="flex gap-2 items-center cursor-pointer"
-          >
-            {isFavorited ? (
-              <div className="flex gap-2 items-center">
-                <StarIcon className="w-4 h-4" /> Unfavorite
-              </div>
-            ) : (
-              <div className="flex gap-2 items-center">
-                <StarHalf className="w-4 h-4" /> Favorite
-              </div>
-            )}
-          </DropdownMenuItem>
-
-          <Protect role="org:admin" fallback={<></>}>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              onClick={() => {
-                if (file.shouldDelete) {
-                  restoreFile({
-                    fileId: file._id,
-                  });
-                } else {
-                  setIsConfirmOpen(true);
-                }
-              }}
-              className="flex gap-2 items-center cursor-pointer"
-            >
-              {file.shouldDelete ? (
-                <div className="flex gap-2 text-green-600 items-center cursor-pointer">
-                  <UndoIcon className="w-4 h-4" /> Restore
-                </div>
-              ) : (
-                <div className="flex gap-2 text-red-600 items-center cursor-pointer">
-                  <TrashIcon className="w-4 h-4" /> Delete
-                </div>
-              )}
-            </DropdownMenuItem>
-          </Protect>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </>
-  );
-}
+import { FileCardActions } from "./file-actions";
 
 export function FileCard({
   file,
-  favorites,
 }: {
-  file: Doc<"files"> & { url: string | null };
-  favorites: Doc<"favorites">[];
+  file: Doc<"files"> & { url: string | null } & { isFavorited: boolean };
 }) {
   const userProfile = useQuery(api.users.getUserProfile, {
     userId: file.userId,
@@ -169,10 +31,6 @@ export function FileCard({
     csv: <GanttChartIcon />,
   } as Record<Doc<"files">["type"], ReactNode>;
 
-  const isFavorited = favorites.some(
-    (favorite) => favorite.fileId === file._id
-  );
-
   return (
     <Card>
       <CardHeader className="relative">
@@ -181,7 +39,7 @@ export function FileCard({
           {file.name}
         </CardTitle>
         <div className="absolute top-2 right-2">
-          <FileCardActions isFavorited={isFavorited} file={file} />
+          <FileCardActions isFavorited={file.isFavorited} file={file} />
         </div>
       </CardHeader>
       <CardContent className="h-[200px] flex justify-center items-center">
