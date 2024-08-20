a7b4e36eee5c3ea263afa033943c141470846d79
antoinevignes <168306302+antoinevignes@users.noreply.github.com>
2024-08-20 20:30:19 +0200

    Added landing page

---
+++ src/app/footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <div className="h-40 mt-12 flex items-center">
+      <div className="container mx-auto flex justify-center items-center gap-8">
+        <div>FileDrive</div>
+        <div> &#169; Antoine Vignes</div>
+      </div>
+    </div>
+  );
+}
